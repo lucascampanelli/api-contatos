13b5050e625fd94c8faa357a88d9e30cc581ead0
Lucas Campanelli <lucas.campanelli@outlook.com>
2024-09-10 23:20:18 -0300

    Construção dos endpoints GET do recurso de pessoas

---
+++ src/api-v1/models/pessoa-model.js
@@ -0,0 +1,40 @@
+function insere() {
+
+}
+
+function lista(params, callback) {
+
+    const objFake = [
+        {
+            "id": 5,
+            "nome": "Francis Ford Copolla",
+            "email": "coppola@hollywood.com",
+            "telefone": "001-124-2111",
+            "organizacao": null,
+            "tags": [
+                "cinema",
+                "trabalho"
+            ]
+        }
+    ];
+
+    const err = null;
+    
+    callback(err, objFake);
+
+}
+
+function altera() {
+
+}
+
+function exclui() {
+
+}
+
+export default {
+    insere,
+    lista,
+    altera,
+    exclui
+}
--- src/api-v1/paths/pessoa-router.js
+++ src/api-v1/paths/pessoa-router.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import pessoaModel from '../models/pessoa-model';
 
 
 
 const pessoaRouter = express.Router();
 
-pessoaRouter.use('/', (req, res, next) => res.send('ENDPOINT PESSOA'));
+// pessoaRouter.use('/', (req, res, next) => res.send('ENDPOINT PESSOA'));
+pessoaRouter.get('/', listarPessoas);
+
+
+
+function listarPessoas(req, res, next) {
+
+    pessoaModel.lista({}, (err, lista) => {
+
+        if(!err)
+            res.json(lista);
+
+        else
+            res.status(400).send(err.message);
+
+    })
+
+}
 
 export default pessoaRouter;
